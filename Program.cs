d3104b3c545a760a4ab273899009411c26a1f794
Mover <movermercene@gmail.com>
2025-06-18 22:09:54 +0800

    Added Database Configurations

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using ADET_FINAL_PROJECT.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ADETPROJECT")));
 
 var app = builder.Build();
 
