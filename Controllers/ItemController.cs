fdaa72e9ee48c3ef361ef1697b95c3ae2fab677b
Mover <movermercene@gmail.com>
2025-06-18 22:08:35 +0800

    Added A Database Connection And Routing For Tables

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using ADET_FINAL_PROJECT.Data;
+using ADET_FINAL_PROJECT.Models;
+using ADET_FINAL_PROJECT.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADET_FINAL_PROJECT.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public ItemController(ApplicationDbContext dbContext)
+        {
+           this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddItemViewModel viewModel)
+        {
+            var item = new Item
+            {
+                Name = viewModel.Name,
+                Category = viewModel.Category,
+                Description = viewModel.Description,
+                Quantity = viewModel.Quantity,
+                Status = "High" // Default value for Status
+            };
+
+            await dbContext.Items.AddAsync(item);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Inventory()
+        {
+            var items = await dbContext.Items.ToListAsync();
+
+            return View(items);
+        }
+    }
+}
